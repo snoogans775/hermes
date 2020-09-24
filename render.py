83b2b72644044d90d03883eb78e4b80e05ca4c08
Kevin Fredericks <snoogans775@gmail.com>
2020-09-24 09:24:47 -0700

    fixed indentation in render.py main loop

--- render.py
+++ render.py
@@ -1,24 +1,16 @@
 import pygame
 
-class Window( object ):
-
-class Icon( object ):
-
-class truck extends icon:
-
-class node extends icon:
-
 while true:
-  pygame.time.delay(100)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN: # if the user hits the mouse button
-                run = False
-                runner.falling = False
-                runner.sliding = False
-                runner.jumpin = False
-        
-        # This will draw text displaying the score to the screen.      
-        win.blit(bg, (0,0))
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN: # if the user hits the mouse button
+            run = False
+            runner.falling = False
+            runner.sliding = False
+            runner.jumpin = False
+
+    # This will draw text displaying the score to the screen.      
+    win.blit(bg, (0,0))
