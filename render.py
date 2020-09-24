d0874495ccebbadbf77e3938ddb09d0c61c5d0c3
Kevin Fredericks <snoogans775@gmail.com>
2020-09-24 09:40:25 -0700

    added test window to render.py

--- render.py
+++ render.py
@@ -1,16 +1,21 @@
 import pygame
 
-while true:
+(width, height) = (300, 200)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+
+bg = pygame.Surface((20, 20))
+
+while True:
     pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
-        if event.type == pygame.MOUSEBUTTONDOWN: # if the user hits the mouse button
-            run = False
-            runner.falling = False
-            runner.sliding = False
-            runner.jumpin = False
+        if event.type == pygame.MOUSEBUTTONDOWN: # if the user hits the mouse 
+            print('Mouse Clicked')
+        
+        print(event)
 
     # This will draw text displaying the score to the screen.      
-    win.blit(bg, (0,0))
+    screen.blit(bg, (0,0))
